025aa1a2303f425122274655c10d904568cd42ab
Haekal Kalipaksi <hkalipaksi@proton.me>
2025-07-16 23:57:08 +0700

    Change to nvim tree

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,7 +1,8 @@
 local fzf = require("fzf-lua")
 
 -- oil
-vim.keymap.set("n", "<leader>e", ":Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>E", ":NvimTreeToggle<CR>", { desc = "Open parent directory" })
 --
 
 -- fzf-lua
@@ -28,5 +29,13 @@ end, { desc = "Fzf search current buffer" })
 vim.keymap.set("n", "<leader>sb", function()
 	fzf.buffers()
 end, { desc = "Fzf search buffer" })
-
 --
+
+-- vim related
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<tab>", ":bnext<CR>", { desc = "[T]ab [N]ext" })
+vim.keymap.set("n", "<s-tab>", ":bprevious<CR>", { desc = "[T]ab [P]revious" })
---
+++ lua/plugins/tree.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			view = {
+				side = "right",
+			},
+			filters = {
+				dotfiles = true,
+				custom = { "^.git$" },
+			},
+		})
+	end,
+}
